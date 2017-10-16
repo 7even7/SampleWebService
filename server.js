1f986ec9c2cc45375b9f49f4a64c5eef73217b77
Patrick Ståhl <patrick.stahl@enfogroup.com>
2017-10-16 09:07:11 +0300

    Basic functionality working. Helpo page stub at / and post-routing that echoes request body back

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var app = express()
+var port = 3000
+var bodyParser = require('body-parser');
+app.use(bodyParser.text());
+
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname +'//public//index.html')
+})
+
+app.get('/test', (request, response)=>{
+    response.send('Hello there')
+})
+app.post('/post', (request, response)=>{
+    console.log(request.body)
+    response.send(request.body +  "  10-4")
+})
+
+app.listen(port, ()=>{
+    console.log("Express app listening on port: " + port)
+})
+
